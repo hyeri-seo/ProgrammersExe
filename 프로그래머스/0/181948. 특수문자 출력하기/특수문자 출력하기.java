588561cc250464eaf55e494c1fac62c394a4a060
hyeri-seo <141993427+hyeri-seo@users.noreply.github.com>
2024-02-28 10:17:39 +0900

    [level 0] Title: 특수문자 출력하기, Time: 142.03 ms, Memory: 75.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181948. 특수문자 출력하기/특수문자 출력하기.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        String str1 = "!@#$%^&*(\\'";
+        char ch = '"';
+        String str2 = "<>?:;";
+        System.out.print(str1 + ch + str2);
+    }
+}
