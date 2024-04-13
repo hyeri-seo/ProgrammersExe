cea66b07a467203cb2509e23d49aa7873546684c
hyeri-seo <141993427+hyeri-seo@users.noreply.github.com>
2024-04-13 22:18:26 +0900

    [level 0] Title: 짝수의 합, Time: 0.02 ms, Memory: 74.8 MB -BaekjoonHub

--- 프로그래머스/0/120831. 짝수의 합/짝수의 합.java
+++ 프로그래머스/0/120831. 짝수의 합/짝수의 합.java
@@ -1,11 +1,11 @@
 class Solution {
     public int solution(int n) {
-        int sum = 0;
-        for(int i=0; i<=n; i++) {
+        int answer = 0;
+        for(int i = 0;i <= n;i++) {
             if(i % 2 == 0) {
-                sum += i;
+                answer += i;
             }
         }
-        return sum;
+        return answer;
     }
 }
