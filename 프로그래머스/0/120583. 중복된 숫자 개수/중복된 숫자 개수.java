357e027ec2c97d6176735ad780b610eba67edb66
hyeri-seo <141993427+hyeri-seo@users.noreply.github.com>
2024-04-14 17:35:10 +0900

    [level 0] Title: 중복된 숫자 개수, Time: 0.02 ms, Memory: 72.9 MB -BaekjoonHub

--- 프로그래머스/0/120583. 중복된 숫자 개수/중복된 숫자 개수.java
+++ 프로그래머스/0/120583. 중복된 숫자 개수/중복된 숫자 개수.java
@@ -1,9 +1,9 @@
 class Solution {
     public int solution(int[] array, int n) {
-        int count = 0;
-        for(int i=0; i<array.length; i++) {
-            if(array[i] == n) count++;
-        }
-        return count;
+        int answer = 0;
+        for(int i=0; i<array.length; i++)
+            if(array[i] == n)
+                answer++;
+        return answer;
     }
 }
